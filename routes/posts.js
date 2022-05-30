f237e2f8870509fa137a485b17717a72d6718a2b
hung <ff789456741@gmail.com>
2022-05-31 01:29:36 +0800

    feat: 統一檢查ID

--- routes/posts.js
+++ routes/posts.js
@@ -2,17 +2,25 @@ const express = require('express');
 const router = express.Router();
 const postsControllers = require('../controllers/posts');
 const handleErrorAsync = require('../service/handleErrorAsync');
+const { isAuth } = require('../service/auth');
+const { checkPostId } = require('../service/checkId');
 
-router.get('/posts', handleErrorAsync(postsControllers.getPosts));
+// 取得所有貼文
+router.get('/posts', isAuth, handleErrorAsync(postsControllers.getPosts));
 
-router.get('/post/:id', handleErrorAsync(postsControllers.getPost));
+// 取得單一貼文
+router.get('/post/:id', isAuth, checkPostId, handleErrorAsync(postsControllers.getPost));
 
-router.post('/post', handleErrorAsync(postsControllers.createdPost));
+// 新增貼文
+router.post('/post', isAuth, handleErrorAsync(postsControllers.createdPost));
 
-router.delete('/posts', handleErrorAsync(postsControllers.deletePosts));
+// 刪除所有貼文
+router.delete('/posts', isAuth, handleErrorAsync(postsControllers.deletePosts));
 
-router.delete('/post/:id', handleErrorAsync(postsControllers.deletePost));
+// 刪除單一貼文
+router.delete('/post/:id', isAuth, checkPostId, handleErrorAsync(postsControllers.deletePost));
 
-router.patch('/post/:id', handleErrorAsync(postsControllers.updatePost));
+// 修改貼文
+router.patch('/post/:id', isAuth, checkPostId, handleErrorAsync(postsControllers.updatePost));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/users');
+const followControllers = require('../controllers/follow');
 const handleErrorAsync = require('../service/handleErrorAsync');
+const { isAuth } = require('../service/auth');
+const { checkUserId } = require('../service/checkId');
 
-router.get('/users', handleErrorAsync(userControllers.getUsers));
+// 註冊
+router.post('/users/sign_up', handleErrorAsync(userControllers.signup));
 
-router.get('/user/:id', handleErrorAsync(userControllers.getUser));
+// 登入
+router.post('/users/sign_in', handleErrorAsync(userControllers.signin));
 
-router.post('/user', handleErrorAsync(userControllers.createdUser));
+// 更改密碼
+router.post('/users/updatePassword', isAuth, handleErrorAsync(userControllers.updatePassword));
 
-router.delete('/users', handleErrorAsync(userControllers.deleteUsers));
+// 取得個人資料
+router.get('/users/profile', isAuth, handleErrorAsync(userControllers.getProfile));
 
-router.delete('/user/:id', handleErrorAsync(userControllers.deleteUser));
+// 修改個人資料
+router.post('/users/profile', isAuth, handleErrorAsync(userControllers.updateProfile));
 
-router.patch('/user/:id', handleErrorAsync(userControllers.updateUser));
+// 追蹤
+router.post('/users/follows/:id', isAuth, checkUserId, handleErrorAsync(followControllers.postFollow));
+
+// 取消追蹤
+router.delete('/users/follows/:id', isAuth, checkUserId, handleErrorAsync(followControllers.deleteFollow));
+
+// 取得所有用戶
+router.get('/users', isAuth, handleErrorAsync(userControllers.getUsers));
+
+// 取得單一用戶
+router.get('/user/:id', isAuth, checkUserId, handleErrorAsync(userControllers.getUser));
+
+// 新增用戶
+router.post('/user', isAuth, handleErrorAsync(userControllers.createdUser));
+
+// 刪除所有用戶
+router.delete('/users', isAuth, handleErrorAsync(userControllers.deleteUsers));
+
+// 刪除單一用戶
+router.delete('/user/:id', isAuth, checkUserId, handleErrorAsync(userControllers.deleteUser));
+
+// 更改用戶
+router.patch('/user/:id', isAuth, checkUserId, handleErrorAsync(userControllers.updateUser));
 
 module.exports = router;
